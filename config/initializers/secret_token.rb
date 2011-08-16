528d59355c986a10958eb7dd924b8dc6cd2d1043
Daniel Lamando <danopia@danotop.(none)>
2011-08-15 22:32:58 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roommates::Application.config.secret_token = 'f8d599096d95ce28ded59bd0951d7d8a3c0d26deb84c3895945448d8f00335852f7f9b7418e25ebfe5c5373bb6116ade8aafee9a2a7292e3e9d711fb958a5fde'
